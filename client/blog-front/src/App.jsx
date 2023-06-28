e0dd0ab0a11ad384ec806a4f00881b584bc9e52b
arnavsar <56104491+Arnav2300@users.noreply.github.com>
2023-06-28 14:02:14 +0530

    topbar

---
+++ client/blog-front/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Topbar from './components/Topbar'
+import './App.css'
+
+
+export default function App() {
+  return (
+    <>
+      <Topbar />
+    </>
+  );
+}
---
+++ client/blog-front/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        'Black':'#222831',
+        'Grey':'#393E46',
+        'Teal':'#00ADB5',
+        'White':'#EEEEEE',
+        'Navy': '#176B87',
+        'Mint': '#DAFFFB',
+      },
+    },
+  },
+  plugins: [],
+};
